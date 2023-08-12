6b591e687d77e8cb9dde99d1e6ab0aefb918f6b9
mig29pandas <kausthubh.manda@gmail.com>
2023-08-12 12:53:40 +0530

    Sorting Introduced

--- MK4kit.cpp
+++ MK4kit.cpp
@@ -10,6 +10,27 @@ class Linked_List{
     Linked_List* PointerPrevious;
     int size;
 
+    private:
+        bool StandardComparator(T a, T b){return a < b;}
+
+        void mergeSort(T arr[], int l, int r, bool (*func)(T, T) = StandardComparator)
+        {
+            int mid = (l + r)/2;
+            mergeSort(arr, l, mid);
+            mergeSort(arr, mid+1, r);
+
+            //Merging the two parts
+            int k = 0, temp_l = l, temp_r = r;
+            int temporary_array[l-r+1];
+            while(temp_l < mid+1 && temp_r < r+1)
+            {
+                if(func(arr[temp_l], arr[temp_r])) temporary_array[k++] = arr[temp_l++];
+                else temporary_array[k++] = arr[temp_r++];
+            }
+            while(temp_l < mid+1) temporary_array[k++] = arr[temp_l++];
+            while(temp_r < r+1) temporary_array[k++] = arr[temp_r++];
+        }
+
     public:
         Linked_List(int val) {
             data = val;
@@ -61,9 +82,17 @@ class Linked_List{
             return Temp.Data;
         }
 
-        void Sort(){
+        void Sort(int leftPtr = 0, int rightPtr = size - 1, bool (*func)(T, T) = StandardComparator){
             //mergeSort applied
-            
+            if(leftPtr < rightPtr)
+            {
+                Linked_List Temp = *this;
+                T arr[rightPtr - leftPtr + 1];
+                for(int i = 0; i < rightPtr - leftPtr + 1; i++){arr[i] = Temp.Data; Temp = Temp.PointerNext;}
+                mergeSort(arr, 0, rightPtr - leftPtr, func);
+                for(int i = 0; i < rightPtr - leftPtr + 1; i++){Temp.Data = arr[i]; Temp = Temp.PointerNext;}
+            }
+
         }
 };
 
